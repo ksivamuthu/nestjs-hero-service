a747d4410b39aac1fa6b647be911b2dd3a7e2d4e
SivamuthuKumar <sivamuthukumar07@gmail.com>
2018-05-19 22:25:11 -0400

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AnyExceptionFilter } from './exceptionfilters/any-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AnyExceptionFilter());
+
+  const options = new DocumentBuilder()
+    .setTitle('Heroes example')
+    .setDescription('The Heroes API description')
+    .setVersion('1.0')
+    .addTag('heroes', 'CRUD operation of heroes')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
